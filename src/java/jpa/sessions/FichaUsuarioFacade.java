3d5273a6781cb35993a7085c15b6c8eb33c1b20a
ADSI <ADSI@10.72.197.188>
2013-09-03 09:29:44 -0500

    cambios seguimientos proyectos

--- src/java/jpa/sessions/FichaUsuarioFacade.java
+++ src/java/jpa/sessions/FichaUsuarioFacade.java
@@ -4,10 +4,20 @@
  */
 package jpa.sessions;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import jpa.entities.EstadoAprendiz;
+import jpa.entities.FichaCaracterizacion;
 import jpa.entities.FichaUsuario;
+import jpa.entities.FichaUsuario_;
+import jpa.entities.Usuario;
+import jpa.entities.Usuario_;
 
 /**
  *
@@ -27,4 +37,14 @@ public class FichaUsuarioFacade extends AbstractFacade<FichaUsuario> {
         super(FichaUsuario.class);
     }
     
+     public List<FichaUsuario> findByEstado(EstadoAprendiz estado, FichaCaracterizacion ficha) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<FichaUsuario> cq = cb.createQuery(FichaUsuario.class);
+        Root<FichaUsuario> fichaUsuario = cq.from(FichaUsuario.class);        
+        cq.where(cb.and(cb.equal(fichaUsuario.get(FichaUsuario_.fichaCaracterizacion), ficha), 
+                (cb.equal(fichaUsuario.get(FichaUsuario_.idEstadoAprendiz), estado))));
+        TypedQuery<FichaUsuario> q = getEntityManager().createQuery(cq);
+        List<FichaUsuario> results = q.getResultList();
+        return results;
+    }
 }
